3fdc60ee17e0868b09800bcf7ce953b0ad9cf762
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2025-12-27 11:43:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ tags = {
 k = 0.1
 holdings = defaultdict(list)
 
-from flask import Flask, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request, redirect, url_for, render_template
 
 def init():
     global tags
@@ -327,6 +327,7 @@ atexit.register(lambda: scheduler.shutdown())
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
 
